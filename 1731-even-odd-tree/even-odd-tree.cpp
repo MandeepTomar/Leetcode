32ea53bd1997aa027f1d7ecf3499417c00030ee3
MandeepTomar <tomarmandeep58@gmail.com>
2026-09-04 12:31:59 +0530

    Time: 9 ms (38.20%) | Memory: 153.7 MB (60.03%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.cpp
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -6,77 +6,43 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool checkq(queue<TreeNode*> q, int& cnt,int n) {
-        if (q.empty()) {
-            return true;
-        }
-        if (cnt % 2 == 0) {
-            int min_val = q.front()->val;
-            q.pop();
-            n--;
-            while (n--) {
-                if (q.front()->val <= min_val) {
-                    return false;
-                }
-                min_val=q.front()->val;
-                q.pop();
-            
-            }
-
-        } else if (cnt % 2 != 0) {
-            int max_val = q.front()->val;
-            q.pop();
-            n--;
-            while (n--) {
-                if (q.front()->val >= max_val) {
-                    return false;
-                }
-                max_val=q.front()->val;
-                q.pop();
-            }
-        }
-        return true;
-    }
-
     bool isEvenOddTree(TreeNode* root) {
-        if (root == NULL) {
-            return false;
-        }
-        queue<TreeNode*> q;
+        queue<TreeNode* >q;
         q.push(root);
-        int cnt = 0;
-        while (!q.empty()) {
-            int n = q.size();
-            bool oddeven=checkq(q,cnt,n);
-            if(!oddeven){
-                return false;
+        bool even_level=true;
+        while(!q.empty()){
+            int prev;
+            int n=q.size();
+            if(even_level){
+                prev=INT_MIN;
             }
-            while (n--) {
-                TreeNode* curr = q.front();
+            else{
+                prev=INT_MAX;
+            }
+            while(n--){
+                TreeNode* curr=q.front();
                 q.pop();
-                if (cnt % 2 == 0) {
-                    if (curr->val % 2 == 0) {
+                if(even_level && (curr->val<=prev || curr->val%2==0)){
                         return false;
-                    }
-                } else {
-                    if (curr->val % 2 != 0) {
-                        return false;
-                    }
                 }
-                if (curr->left != NULL) {
+                else if(!even_level && (curr->val%2!=0 || curr->val>=prev)){
+                    return false;
+                }
+                if(curr->left!=NULL){
                     q.push(curr->left);
                 }
-                if (curr->right != NULL) {
+                if(curr->right!=NULL){
                     q.push(curr->right);
                 }
+                prev=curr->val;
+
             }
-            cnt++;
+            even_level=!even_level;
         }
         return true;
     }
